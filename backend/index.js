49b5a838c3188aee09ffb24ad73caabe7aac8020
TarunTeja0 <taruntejagurajada@gmail.com>
2024-10-06 20:53:43 +0530

    Second commit for MERN stack project

--- backend/index.js
+++ backend/index.js
@@ -1,9 +1,8 @@
 require('dotenv').config();
 
-const config = require("./config.json");
 const mongoose = require('mongoose');
 
-mongoose.connect(config.connectionString)
+mongoose.connect(process.env.connectionString)
 
 const User = require("./models/userModel");
 const Note = require("./models/noteModel");
