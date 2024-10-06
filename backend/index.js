86dd5e5b86482f54bb3272d1cad1bed2e82334c2
TarunTeja0 <124495772+TarunTeja0@users.noreply.github.com>
2024-10-06 21:53:27 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -18,7 +18,7 @@ const {authenticateToken} = require("./utilities");
 
 app.use(express.json());
 app.use(cors({
-    origin: "http://localhost:3000",
+    origin: "https://web-notes-frontend.onrender.com/",
 }))
 
 app.get("/", (req, res)=>{
